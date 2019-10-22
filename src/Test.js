8b183f6186efeef0d17d4e61842b8b712c192404
Richard Hpa <richard.hpa@bun2-205-14.acg.ac.nz>
2019-10-22 14:54:58 +1300

    demoed states on a div

--- src/Test.js
+++ src/Test.js
@@ -2,10 +2,52 @@ import React, { Component } from 'react';
 import './test.css';
 
 class Test extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      colour: 'red'
+    }
+
+    // this.clicked = this.clicked.bind(this);
+  }
+
+  // clicked(){
+  //   console.log('this is the old way and we need to bind it in the constructor');
+  //   console.log(this.state.colour);
+
+  // if(this.state.colour === 'red'){
+  //   this.setState({
+  //     colour: 'blue'
+  //   })
+  // } else {
+  //   this.setState({
+  //     colour: 'red'
+  //   })
+  // }
+  // }
+
+  clicked = () => {
+    console.log('this is the es6 way');
+    console.log(this.state.colour);
+
+    if(this.state.colour === 'red'){
+      this.setState({
+        colour: 'blue'
+      })
+    } else {
+      this.setState({
+        colour: 'red'
+      })
+    }
+
+  }
+
   render(){
     return (
-      <div>
-        <a href={this.props.href}>{this.props.message}</a>
+      <div className={`link ${this.state.colour}`} onClick={this.clicked}   >
+        {this.state.colour}
+        <a className="anchor" href={this.props.href}>{this.props.message}</a>
       </div>
     )
   }
