465d1c66fbdbc0d6087ae86cd5ce227f406ff9f9
Richard Hpa <richard.hpa@bun2-205-14.acg.ac.nz>
2019-10-29 16:07:24 +1300

    we can add products and view them in our list

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,11 +4,27 @@ import './Form.css';
 import Button from '../Button/Button';
 
 class Form extends Component{
+  constructor(props){
+    super(props);
+
+    this.changeInput = this.changeInput.bind(this);
+  }
+
+  changeInput(e){
+    // console.log(e.target.value);
+    this.props.changeInputValue(e.target.value);
+  }
+
+  submitForm = (e) => {
+    e.preventDefault();
+    // console.log('we are submitting the form');
+    this.props.submit();
+  }
 
   render(){
     return(
-      <form className="searchForm">
-        <input className="searchBar"/>
+      <form className="searchForm" onSubmit={this.submitForm}>
+        <input className="searchBar" value={this.props.item.value} onChange={this.changeInput}/>
         <Button btnText="Add New Item"/>
       </form>
     )
--- src/List/List.js
+++ src/List/List.js
@@ -4,15 +4,84 @@ import Form from '../Form/Form';
 import Item from '../Item/Item';
 
 class List extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      allItems: [
+        {
+          id: 1,
+          text: 'Apples'
+        },
+        {
+          id: 2,
+          text: 'Bananas'
+        },
+        {
+          id: 3,
+          text: 'Pears'
+        },
+        {
+          id: 4,
+          text: 'Beers'
+        }
+      ],
+      currentItem: {
+        id: null,
+        value: ''
+      }
+    }
+  }
+
+  handleChangeInputValue = (text) => {
+    let { currentItem } = this.state;
+    currentItem.value = text;
+    this.setState({
+      currentItem: currentItem
+    })
+    // console.log('the input value has been changed from Form.js');
+    // console.log(text);
+  }
+
+  handleSubmit = () => {
+    let { allItems, currentItem } = this.state;
+
+    const newItem = {
+      id: allItems.length + 1,
+      text: currentItem.value
+    }
+
+    allItems.push(newItem);
+
+    this.setState({
+      currentItem: {
+        id: null,
+        value: ''
+      },
+      allItems: allItems
+    })
+
+  }
 
   render(){
+    console.log(this.state.allItems);
     return(
       <div className="listContainer">
         <h1>Shopping List</h1>
-        <Form />
+        <Form
+          item={this.state.currentItem}
+          changeInputValue={this.handleChangeInputValue}
+          submit={this.handleSubmit}
+         />
         <div className="list">
-          <Item />
-          <Item />
+          {
+            this.state.allItems.map(singleItem => {
+              return <Item
+                key={singleItem.id}
+                itemInfo={singleItem}
+                />
+            })
+          }
         </div>
       </div>
     )
